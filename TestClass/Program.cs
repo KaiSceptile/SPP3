7cd312bf45fa8e12619d61c020201372bc0d3078
KaiSceptile <90106146+KaiSceptile@users.noreply.github.com>
2022-11-29 16:42:59 +0300

    adding tests

---
+++ TestClass/Program.cs
@@ -0,0 +1,32 @@
+﻿﻿using System.IO;
+ using AssemblyBrowserLib;
+
+namespace TestClass
+{
+    internal static class Program
+    {
+        private static readonly AssemblyBrowser Browser = new AssemblyBrowser();
+        
+        public static void Main(string[] args)
+        {
+            
+            var assemblyInfo = Browser.GetAssemblyInfo("E:\\Sharaga\\SPP\\A\\AssemblyBrowser\\Tests\\Resources\\TestClass.exe");
+            
+        }
+        
+    }
+    
+    public static class StringExtension
+    {
+        public static int CharCount(this string str, char c)
+        {
+            int counter = 0;
+            for (int i = 0; i<str.Length; i++)
+            {
+                if (str[i] == c)
+                    counter++;
+            }
+            return counter;
+        }
+    }
+}
